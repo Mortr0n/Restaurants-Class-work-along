8f9d40ef875b67321927eadb88312515addc52bd
Mortron <cmsl4y3r@gmail.com>
2021-12-10 16:02:11 -0700

    create mostly complete

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 // Set up server.js with mongoose.config to start
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 require('./config/mongoose.config');
 
+app.use(cors());
 app.use(express.json(), express.urlencoded({ extended: true }));
 
 
